8546d7c841226a05d4a505586881b7ec9d49814a
Eagle <kuzmenkoav@rambler.ru>
2011-09-30 04:06:26 +0400

    Forgotten file.

---
+++ XNA/XNA/model/behavior/Behavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XNA.model.grid;
+
+namespace XNA.model.behavior
+{
+    abstract public class Behavior
+    {
+        private bool _enabled = false;
+
+        public bool Enabled
+        {
+            get { return _enabled; }
+            protected set { _enabled = value; }
+        }
+
+        public virtual void Disable()
+        {
+            _enabled = false;
+        }
+
+        public virtual void Enable(ActiveObject caller)
+        {
+            _enabled = true;
+        }
+    }
+}
